18fbc0efdda61c9679e29d3c8681294c90384484
Zoey <zoey.zhang@aolc.cn>
2021-07-23 15:08:41 +0800

    Hello name

---
+++ src/components/Hello.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export interface Props {
+	name: string;
+	enthusiasmLevel?: number;
+}
+
+function getExclamationMarks(numChars: number) {
+	return Array(numChars + 1).join('!');
+}
+
+export class Hello extends Component<Props, object> {
+	render() {
+		const {name, enthusiasmLevel = 1} = this.props;
+
+		if (enthusiasmLevel <= 0) {
+			throw new Error('You could be a little more enthusiastic. :D');
+		}
+
+		return (
+			<div className="hello">
+				<div className="greeting">
+					Hello {name + getExclamationMarks(enthusiasmLevel)}
+				</div>
+			</div>
+		);
+	}
+}
